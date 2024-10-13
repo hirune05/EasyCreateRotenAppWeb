a3cb585b552f71153f4ad6e10538447def95ae93
f-yusei <fkys2932@gmail.com>
2024-10-13 10:06:33 +0900

    fix: portの間違いを修正

--- backend/app/server/server.go
+++ backend/app/server/server.go
@@ -12,17 +12,7 @@ import (
 	"strconv"
 	"syscall"
 	"time"
-
-	"github.com/labstack/echo/v4"
 )
-
-func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
-}
 
 func Run() error {
 	// db, err := infrastructure.NewDB(config.MySQLConfig().FormatDSN())
