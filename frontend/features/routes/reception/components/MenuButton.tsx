2532732297bf71471defe0f224f8e2e6c930af7d
akito-0520 <n.akito.h17@icloud.com>
2024-10-22 16:18:57 +0900

    feat: "メニューを表示するコンポーネントの見た目を変更"

--- frontend/features/routes/reception/components/MenuButton.tsx
+++ frontend/features/routes/reception/components/MenuButton.tsx
@@ -5,11 +5,11 @@ import MinusButton from "./MinusButton"
 function MenuButton() {
   return (
     <Card>
-      <CardContent className="flex">
-        <h1>メニュー1</h1>
-        <PlusButton/>
-        <h2>0</h2>
-        <MinusButton/>
+      <CardContent className='flex justify-between items-center p-4 border rounded-md bg-gray-100'>
+        <div className='text-lg text-black'>メニュー1</div>
+        <PlusButton />
+        <span className='px-3 py-1 text-black'>0</span>
+        <MinusButton />
       </CardContent>
     </Card>
   )
