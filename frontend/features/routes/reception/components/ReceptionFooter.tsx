0502294df3fe0e26a742c991f40f764737e6ae90
asushidao <acepokemo@icloud.com>
2024-10-23 00:41:24 +0900

    feat/add_Cartmenu

--- frontend/features/routes/reception/components/ReceptionFooter.tsx
+++ frontend/features/routes/reception/components/ReceptionFooter.tsx
@@ -1,48 +1,22 @@
 import React from 'react'
 import { FooterDrawerComponent } from '@/components/footer-drawer'
-import CrossButton from '@/features/common/button/components/CrossButton'
-import { Input } from '@/components/ui/input'
+import CartMenu from './CartMenu'
 import Footer from './Footer'
 
 function ReceptionFooter() {
   return (
-    <>
-      <FooterDrawerComponent>
-        <div className='p-4 max-w-sm mx-auto'>
-          <div className='space-y-2 flex-col'>
-            <div className='flex justify-between items-center border rounded-md bg-gray-100'>
-              <span className='text-lg text-black'>メニュー 1</span>
-              <span className='px-3 py-1 text-black'>(2)</span>
-              <CrossButton />
-            </div>
-            <div>
-              <Input type='text' placeholder='備考' />
-            </div>
-          </div>
-          <div className='space-y-2 flex-col'>
-            <div className='flex justify-between items-center border rounded-md bg-gray-100'>
-              <span className='text-lg text-black'>メニュー 2</span>
-              <span className='px-3 py-1 text-black'>(1)</span>
-              <CrossButton />
-            </div>
-            <div>
-              <Input type='text' placeholder='備考' />
-            </div>
-          </div>
-          <div className='space-y-2 flex-col'>
-            <div className='flex justify-between items-center border rounded-md bg-gray-100'>
-              <span className='text-lg text-black'>メニュー 3</span>
-              <span className='px-3 py-1 text-black'>(4)</span>
-              <CrossButton />
-            </div>
-            <div>
-              <Input type='text' placeholder='備考' />
-            </div>
-          </div>
+    <div className='flex flex-col min-h-screen'>
+      <div className="flex-grow">
+      <FooterDrawerComponent >
+        <div className='p-4 max-w-sm mx-auto overflow-scroll'>
+          <CartMenu/>
+          <CartMenu/>
+          <CartMenu/>
         </div>
       </FooterDrawerComponent>
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
 
