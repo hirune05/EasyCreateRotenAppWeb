d6d8b53f1648e518b6ed179348f11cd54ae5fdd0
akito-0520 <n.akito.h17@icloud.com>
2024-10-22 16:19:45 +0900

    feat: "footerの見た目を変更"

--- frontend/features/routes/reception/components/Footer.tsx
+++ frontend/features/routes/reception/components/Footer.tsx
@@ -1,16 +1,18 @@
-import { Button } from '@/components/ui/button';
-import React from 'react';
+import Link from 'next/link'
+import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-200 fixed bottom-0 left-0 right-0 p-4 h-[60px] flex justify-between items-center">
+    <div className='bg-gray-200 fixed bottom-0 left-0 right-0 p-4 h-[60px] flex justify-between items-center'>
       <div className='flex space-x-4'>
         <p className="text-sm text-gray-800">通常:1 券:2</p>
         <p className="text-sm text-gray-800">計:200円</p>
       </div>
-      <Button className='bg-green-800 text-gray-800'>会計</Button>
-    </footer>
-  );
-};
+      <Link className='bg-green-800 text-gray-800' href='/casher'>
+        会計
+      </Link>
+    </div>
+  )
+}
 
 export default Footer;
--- frontend/features/routes/reception/components/ReceptionFooter.tsx
+++ frontend/features/routes/reception/components/ReceptionFooter.tsx
@@ -1,23 +1,43 @@
 import React from 'react'
 import { FooterDrawerComponent } from '@/components/footer-drawer'
+import CrossButton from '@/features/common/button/components/CrossButton'
+import { Input } from '@/components/ui/input'
 import Footer from './Footer'
 
 function ReceptionFooter() {
   return (
     <>
-    <FooterDrawerComponent>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div>
-            <h2 className="font-semibold mb-2">About Us</h2>
-            <p>Learn more about our company and mission.</p>
+      <FooterDrawerComponent>
+        <div className='p-4 max-w-sm mx-auto'>
+          <div className='space-y-2 flex-col'>
+            <div className='flex justify-between items-center border rounded-md bg-gray-100'>
+              <span className='text-lg text-black'>メニュー 1</span>
+              <span className='px-3 py-1 text-black'>(2)</span>
+              <CrossButton />
+            </div>
+            <div>
+              <Input type='text' placeholder='備考' />
+            </div>
           </div>
-          <div>
-            <h2 className="font-semibold mb-2">Contact</h2>
-            <p>Get in touch with our support team.</p>
+          <div className='space-y-2 flex-col'>
+            <div className='flex justify-between items-center border rounded-md bg-gray-100'>
+              <span className='text-lg text-black'>メニュー 2</span>
+              <span className='px-3 py-1 text-black'>(1)</span>
+              <CrossButton />
+            </div>
+            <div>
+              <Input type='text' placeholder='備考' />
+            </div>
           </div>
-          <div>
-            <h2 className="font-semibold mb-2">Legal</h2>
-            <p>View our terms of service and privacy policy.</p>
+          <div className='space-y-2 flex-col'>
+            <div className='flex justify-between items-center border rounded-md bg-gray-100'>
+              <span className='text-lg text-black'>メニュー 3</span>
+              <span className='px-3 py-1 text-black'>(4)</span>
+              <CrossButton />
+            </div>
+            <div>
+              <Input type='text' placeholder='備考' />
+            </div>
           </div>
         </div>
     </FooterDrawerComponent>
