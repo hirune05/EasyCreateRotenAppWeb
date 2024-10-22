b96a5c107dd92ae6e96e42882c3b90f23c7b2b1a
akito-0520 <n.akito.h17@icloud.com>
2024-10-22 16:20:06 +0900

    feat: "受付画面の見た目を作成"

--- frontend/app/reception/page.tsx
+++ frontend/app/reception/page.tsx
@@ -1,10 +1,19 @@
-import ReceptionFooter from "@/features/routes/reception/components/ReceptionFooter";
+import MenuButton from '@/features/routes/reception/components/MenuButton'
+import ReceptionFooter from '@/features/routes/reception/components/ReceptionFooter'
 
 export default function Page() {
   return (
-    <div> 
-      <div className="h-auto">hello</div>
-      <ReceptionFooter/>
-    </div>
+    <>
+      <div className='p-4 max-w-sm mx-auto'>
+        <div className='space-y-2'>
+          <MenuButton />
+          <MenuButton />
+          <MenuButton />
+          <MenuButton />
+          <MenuButton />
+        </div>
+        <ReceptionFooter />
+      </div>
+    </>
   )
 }
