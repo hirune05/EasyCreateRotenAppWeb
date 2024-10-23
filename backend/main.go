605a9e9bac919f7ebd8d6aaa7ceee352464bd146
f-yusei <fkys2932@gmail.com>
2024-10-24 00:22:49 +0900

    fix: envファイルが読み込まれるように

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"backend/app/server"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+
 	log.Fatalf("%+v", server.Run())
 }
