729bf12f6eed1c5d9ad1df0f1906197e259efa68
barley2tea <okity1024_0kity256y@icloud.com>
2024-10-24 01:20:02 +0900

    feat Login時にStoreStaffIDを追加#69

--- backend/app/domain/repository/student.go
+++ backend/app/domain/repository/student.go
@@ -8,8 +8,14 @@ import (
 	"gorm.io/gorm"
 )
 
+type CheckInEventDTO struct {
+        StoreID       int
+        StoreName     string
+        StoreStaffID  int
+}
+
 type StudentRepository interface {
 	Create(ctx context.Context, tx *gorm.DB, acc *object.Student) error
 	FindByStudentId(ctx context.Context, studentId int) (*object.Student, error)
-        CheckInEvent(ctx context.Context, event_id int, student_id int) (*object.Store, error)
+        CheckInEvent(ctx context.Context, event_id int, student_id int) (*CheckInEventDTO, error)
 }
