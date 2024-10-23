70681c00645fdcaf28deb01a1f92085b97c32da1
akito-0520 <n.akito.h17@icloud.com>
2024-10-23 11:50:40 +0900

    feat: "注文内容を表示するコンポーネントの追加 #54"

--- frontend/features/routes/delivery/components/DeliveryItem.tsx
+++ frontend/features/routes/delivery/components/DeliveryItem.tsx
@@ -2,11 +2,13 @@ import MenuItems from './MenuItems'
 
 const DeliveryItem = () => {
   return (
-    <div>
-      {/* 注文者名 */}
-      <p>あすし</p>
-      {/* 受け取り番号 */}
-      <h3>3</h3>
+    <div className='text-2xl border space-y-4'>
+      <div className='flex justify-between items-center mb-4'>
+        {/* 注文者名 */}
+        <p className='text-sm'>あすし</p>
+        {/* 受け取り番号 */}
+        <p className='text-sm'>3</p>
+      </div>
       <MenuItems />
     </div>
   )
