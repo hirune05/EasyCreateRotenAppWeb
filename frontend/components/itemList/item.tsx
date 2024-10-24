2e866bfbda638b4248fc48f205ed272b5a17a056
akito-0520 <n.akito.h17@icloud.com>
2024-10-24 18:17:21 +0900

    feat: "liタグにユニークなkeyをつける機構の作成"

--- frontend/components/itemList/item.tsx
+++ frontend/components/itemList/item.tsx
@@ -1,12 +1,14 @@
 import { Item } from '@/types/type'
+import { v4 as uuidv4 } from 'uuid'
 
 type ItemProp = {
   item: Item
 }
 
 const ItemComponent = ({ item }: ItemProp) => {
+  const uniqueID = uuidv4()
   return (
-    <li className='flex justify-between'>
+    <li className='flex justify-between' key={uniqueID}>
       <div>
         <span>{item.name}</span>
         <span>1</span>
