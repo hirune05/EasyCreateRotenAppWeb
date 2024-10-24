98ce98803ed4ee8917614c00b2a25f78083f3630
akito-0520 <n.akito.h17@icloud.com>
2024-10-25 00:38:01 +0900

    feat: "お店の商品をグローバルから取得して表示する機能 #93"

--- frontend/features/routes/reception/components/MenuButton.tsx
+++ frontend/features/routes/reception/components/MenuButton.tsx
@@ -1,15 +1,33 @@
 import { Card, CardContent } from '@/components/ui/card'
-import PlusButton from '@/features/routes/reception/components/PlusButton'
+import { Item } from '@/types/type'
+import PlusButton from './PlusButton'
 import MinusButton from './MinusButton'
+import { useAtom } from 'jotai'
+import { cartItemsAtom } from '@/utils/globalState'
+import { useEffect, useState } from 'react'
 
-function MenuButton() {
+type ItemProp = {
+  storeItem: Item
+}
+
+const MenuButton: React.FC<ItemProp> = ({ storeItem }) => {
+  const [cartItems] = useAtom(cartItemsAtom)
+  const [itemCount, useItemCount] = useState(0)
+
+  // Todo
+  // useEffectの内容をhooksにまとめる
+  useEffect(() => {
+    const itemNum = cartItems.filter(item => item.name == storeItem.name).length
+    useItemCount(itemNum)
+  }, [cartItems])
+
   return (
     <Card>
       <CardContent className='flex justify-between items-center p-4 border rounded-md bg-gray-100'>
-        <div className='text-lg text-black'>メニュー1</div>
-        <PlusButton />
-        <span className='px-3 py-1 text-black'>0</span>
-        <MinusButton />
+        <div className='text-lg text-black'>{storeItem.name}</div>
+        <PlusButton item={storeItem} />
+        <span className='px-3 py-1 text-black'>{itemCount}</span>
+        <MinusButton item={storeItem} />
       </CardContent>
     </Card>
   )
