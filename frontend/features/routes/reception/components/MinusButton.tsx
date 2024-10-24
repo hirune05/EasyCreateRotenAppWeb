5d3f836991f9aab7c4e61cd82f5735f3619459d7
akito-0520 <n.akito.h17@icloud.com>
2024-10-25 00:34:18 +0900

    feat: "商品の個数を変更する機能の作成 #93"

--- frontend/features/routes/reception/components/MinusButton.tsx
+++ frontend/features/routes/reception/components/MinusButton.tsx
@@ -1,9 +1,29 @@
 import SkeletonButton from '@/components/skeletonButton'
+import { Item } from '@/types/type'
+import { cartItemsAtom } from '@/utils/globalState'
+import { useAtom } from 'jotai'
 import { Minus } from 'lucide-react'
 
-export default function MinusButton() {
+type MinusButtonProp = {
+  item: Item
+}
+
+export default function MinusButton({ item }: MinusButtonProp) {
+  const [cartItems, setCartItems] = useAtom(cartItemsAtom)
+
+  // Todo
+  // handleDeleteItemをhooksにまとめる
+  const handleDeleteItem = () => {
+    const lastIndex = cartItems.lastIndexOf(item)
+    if (lastIndex !== -1) {
+      const updatedItems = [...cartItems]
+      updatedItems.splice(lastIndex, 1)
+      setCartItems(updatedItems)
+    }
+  }
+
   return (
-    <SkeletonButton>
+    <SkeletonButton handleFunction={handleDeleteItem}>
       <Minus className='h-6 w-6' />
     </SkeletonButton>
   )
--- frontend/features/routes/reception/components/PlusButton.tsx
+++ frontend/features/routes/reception/components/PlusButton.tsx
@@ -1,10 +1,24 @@
 import { Plus } from 'lucide-react'
-
 import SkeletonButton from '@/components/skeletonButton'
+import { Item } from '@/types/type'
+import { useSetAtom } from 'jotai'
+import { cartItemsAtom } from '@/utils/globalState'
 
-export default function PlusButton() {
+type plusButtonProp = {
+  item: Item
+}
+
+export default function PlusButton({ item }: plusButtonProp) {
+  const setCartItems = useSetAtom(cartItemsAtom)
+
+  // Todo
+  // handlePlusItemをhooksにまとめる
+  const handleAddItem = () => {
+    setCartItems((prevItems: Item[]) => [...prevItems, item])
+  }
+
   return (
-    <SkeletonButton>
+    <SkeletonButton handleFunction={handleAddItem}>
       <Plus className='h-6 w-6' />
     </SkeletonButton>
   )
