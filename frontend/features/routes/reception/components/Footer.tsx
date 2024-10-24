df6841715ae125a7749e0df0b0ab6737919f8d36
f-yusei <fkys2932@gmail.com>
2024-10-25 04:47:52 +0900

    chore: format

--- frontend/features/routes/reception/components/Footer.tsx
+++ frontend/features/routes/reception/components/Footer.tsx
@@ -6,16 +6,23 @@ import type React from 'react'
 
 const Footer: React.FC = () => {
   const [cartItems] = useAtom(cartItemsAtom)
-  const advSaleItems = cartItems.filter((cartItems) => cartItems.name.includes('前売り券'));
-  const usuSaleItems = cartItems.filter((cartItems) => !cartItems.name.includes('前売り券'));
-  const totalPrice = usuSaleItems.reduce((sum, usuSaleItems) => sum + usuSaleItems.price, 0);
-  
-
+  const advSaleItems = cartItems.filter(cartItems =>
+    cartItems.name.includes('前売り券'),
+  )
+  const usuSaleItems = cartItems.filter(
+    cartItems => !cartItems.name.includes('前売り券'),
+  )
+  const totalPrice = usuSaleItems.reduce(
+    (sum, usuSaleItems) => sum + usuSaleItems.price,
+    0,
+  )
 
   return (
     <div className='bg-gray-200 fixed bottom-0 position-absolute left-0 right-0 p-4 h-[60px] flex justify-between items-center '>
       <div className='flex flex-col space-x-4'>
-        <p className='text-lg ml-4  text-gray-800'>通常:{usuSaleItems.length} 券:{advSaleItems.length}</p>
+        <p className='text-lg ml-4  text-gray-800'>
+          通常:{usuSaleItems.length} 券:{advSaleItems.length}
+        </p>
         <p className='text-lg  text-gray-800'>計:{totalPrice}</p>
       </div>
 
