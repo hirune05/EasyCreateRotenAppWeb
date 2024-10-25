26bfde0dfa22512d061f7172e22aed65c1743a4c
akito-0520 <n.akito.h17@icloud.com>
2024-10-25 15:29:32 +0900

    feat: "hooksにまとめて簡潔にした"

--- frontend/features/routes/reception/components/Footer.tsx
+++ frontend/features/routes/reception/components/Footer.tsx
@@ -1,30 +1,24 @@
-import { useAtom } from 'jotai'
-import { cartItemsAtom } from '@/utils/globalState'
 import type React from 'react'
 import PaymentButton from './PaymentButton'
 import { Suspense } from 'react'
+import useCountAdvSaleItems from '@/hooks/useCountAdvSaleItems'
+import useCountTotalPrice from '@/hooks/useCountTotalPrice'
+import { useAtom } from 'jotai'
+import { cartItemsAtom } from '@/utils/globalState'
 
 const Footer: React.FC = () => {
   const [cartItems] = useAtom(cartItemsAtom)
-  const advSaleItems = cartItems.filter(cartItems =>
-    cartItems.name.includes('前売り券'),
-  )
-  const usuSaleItems = cartItems.filter(
-    cartItems => !cartItems.name.includes('前売り券'),
-  )
-
-  const totalPrice = usuSaleItems.reduce(
-    (sum, usuSaleItems) => sum + (usuSaleItems.price ?? 0),
-    0,
-  )
+  const advSaleItems = useCountAdvSaleItems()
+  const totalPrice = useCountTotalPrice()
 
   return (
     <div className='bg-gray-200 fixed bottom-0 position-absolute left-0 right-0 p-4 h-[60px] flex justify-between items-center '>
       <div className='flex flex-col space-x-4'>
         <p className='text-xl ml-4 font-bold text-gray-800'>
-          通常:{usuSaleItems.length} 券:{advSaleItems.length}
+          通常: {cartItems.length - advSaleItems.length} 点, 券:{' '}
+          {advSaleItems.length} 点
         </p>
-        <p className='text-xl font-bold text-gray-800'>計:{totalPrice}</p>
+        <p className='text-xl font-bold text-gray-800'>計: {totalPrice} 円</p>
       </div>
       <Suspense fallback={<div>Loading...</div>}>
         <PaymentButton />
