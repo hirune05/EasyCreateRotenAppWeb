1eebc8275291fcb6128b191fe68654c32ef6dd55
akito-0520 <n.akito.h17@icloud.com>
2024-10-25 12:16:01 +0900

    feat: "OrderDetailModalをsuspenseでラップ"

--- frontend/app/reception/page.tsx
+++ frontend/app/reception/page.tsx
@@ -1,11 +1,14 @@
 import MenuButtons from '@/features/routes/reception/components/MenuButtons'
 import OrderDetailModal from '@/features/routes/reception/components/orderDetailmodal'
 import ReceptionFooter from '@/features/routes/reception/components/ReceptionFooter'
+import { Suspense } from 'react'
 
 export default function Page() {
   return (
     <>
-      <OrderDetailModal />
+      <Suspense fallback={<div>Loading...</div>}>
+        <OrderDetailModal />
+      </Suspense>
       <div className='p-4 max-w-sm mx-auto'>
         <div className='space-y-2'>
           <MenuButtons />
