3baf89ff06fb71e9e6880cd64a39a0bf4be52dfb
akito-0520 <n.akito.h17@icloud.com>
2024-10-25 12:14:48 +0900

    feat: "カートの内容を送信するhooksの追加"

--- frontend/features/routes/casher/hooks.ts
+++ frontend/features/routes/casher/hooks.ts
@@ -1,21 +1,43 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { addOrderComplex } from '../delivery/endpoint'
-import { AddOrderComplexRequest, Order } from '@/types/type'
+import { AddOrderComplexRequest, OrderedItem } from '@/types/type'
 import { useRouter } from 'next/navigation'
+import { useAtom, useSetAtom } from 'jotai'
+import { cartItemsAtom, storeItemsAtom } from '@/utils/globalState'
 
 export const useSubmitCart = () => {
   const router = useRouter()
   const [reqData, setReqData] = useState<AddOrderComplexRequest>()
+  const setCartItems = useSetAtom(cartItemsAtom)
+  const [storeItems] = useAtom(storeItemsAtom)
+
+  const totalPrice = 900
+  const paymentAmount = 1000
+
   useEffect(() => {
     const postCartItems = async () => {
       if (reqData) {
         await addOrderComplex(reqData)
 
-        // const query = {
-        //   items: reqData.items,
-        // }
-        router.push('reception')
+        const itemData: OrderedItem[] = reqData.items.map(reqItem => {
+          const foundItem = storeItems.find(item => item.id === reqItem.item_id)
+          return {
+            name: foundItem ? foundItem.name : 'Unknown item name',
+            quantity: reqItem.quantity,
+            arranges: reqItem.arranges,
+          }
+        })
+
+        const path =
+          'reception?totalPrice=' +
+          String(totalPrice) +
+          '&paymentAmount=' +
+          String(paymentAmount) +
+          '&itemData=' +
+          JSON.stringify(itemData)
+        setCartItems([])
+        router.push(path)
       }
     }
     postCartItems()
