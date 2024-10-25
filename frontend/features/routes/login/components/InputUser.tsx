75c24d589ec189ec36a62680fd7a42909e0ccc8b
f-yusei <fkys2932@gmail.com>
2024-10-25 14:04:20 +0900

    fix: 数字入力のキーボードが出るように

--- frontend/features/routes/login/components/InputUser.tsx
+++ frontend/features/routes/login/components/InputUser.tsx
@@ -36,6 +36,7 @@ const InputUser: FC<InputUserProps> = ({
           <input
             type='text'
             id='studentId'
+            inputMode='numeric'
             value={studentId}
             onChange={e => setStudentId(e.target.value)}
             className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
