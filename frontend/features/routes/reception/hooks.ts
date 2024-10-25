9b934b6fb7a758a8d4afea995b24e2ba4c149fe2
akito-0520 <n.akito.h17@icloud.com>
2024-10-25 15:31:00 +0900

    feat: "paramから値を受け取る部分をhooks化"

--- frontend/features/routes/reception/hooks.ts
+++ frontend/features/routes/reception/hooks.ts
@@ -1,6 +1,7 @@
 'use client'
-
-import { useRouter } from 'next/navigation'
+import { OrderedItem } from '@/types/type'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export const useHandleCliclkPayment = () => {
   const router = useRouter()
@@ -9,4 +10,36 @@ export const useHandleCliclkPayment = () => {
     router.push('casher')
   }
   return { handleClick }
+}
+
+export const useGetParams = () => {
+  const [orderedItems, setOrderedItems] = useState<OrderedItem[]>()
+  const [totalPrice, setTotalPrice] = useState<number>(0)
+  const [paymentAmount, setPaymentAmount] = useState<number>(0)
+  const [totalItems, setTotalItems] = useState<number>(0)
+  const [advSaleItems, setAdvSaleItems] = useState<number>(0)
+  const searchParams = useSearchParams()
+  useEffect(() => {
+    const getItemDataParams = searchParams.get('itemData')
+    const getTotalPriceParams = searchParams.get('totalPrice')
+    const getPaymentAmountParams = searchParams.get('paymentAmount')
+    const getTotalItemsParams = searchParams.get('totalItems')
+    const getAdvSaleItemsParams = searchParams.get('advSaleItems')
+
+    if (
+      getItemDataParams &&
+      getTotalPriceParams &&
+      getPaymentAmountParams &&
+      getTotalItemsParams &&
+      getAdvSaleItemsParams
+    ) {
+      setOrderedItems(JSON.parse(getItemDataParams))
+      setTotalPrice(Number(getTotalPriceParams))
+      setPaymentAmount(Number(getPaymentAmountParams))
+      setTotalItems(Number(getTotalItemsParams))
+      setAdvSaleItems(Number(getAdvSaleItemsParams))
+    }
+  }, [searchParams])
+
+  return { orderedItems, totalPrice, paymentAmount, totalItems, advSaleItems }
 }
