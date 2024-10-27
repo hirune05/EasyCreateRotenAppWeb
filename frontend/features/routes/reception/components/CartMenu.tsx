8f5943eba42c5c64ee9791444144abe7e9b1bbf6
akito-0520 <n.akito.h17@icloud.com>
2024-10-27 11:20:04 +0900

    feat: "備考欄に文字を入力できるようにした"

--- frontend/features/routes/reception/components/CartMenu.tsx
+++ frontend/features/routes/reception/components/CartMenu.tsx
@@ -1,6 +1,10 @@
+'use client'
 import { Input } from '@/components/ui/input'
 import CrossButton from '@/features/routes/reception/components/CrossButton'
 import type { Item } from '@/types/type'
+import { cartItemsAtom } from '@/utils/globalState'
+import { useAtom, useSetAtom } from 'jotai'
+import { useEffect, useState } from 'react'
 
 type CartMenuProps = {
   item: Item
@@ -8,6 +12,26 @@ type CartMenuProps = {
 }
 
 function CartMenu({ item, index }: CartMenuProps) {
+  const [description, setDescription] = useState<string>(
+    item.description ? item.description : '',
+  )
+  const [cartItems, setCartItems] = useAtom(cartItemsAtom)
+
+  useEffect(() => {
+    setDescription(item.description ? item.description : '')
+  }, [cartItems])
+
+  const handleChange = (value: string) => {
+    const newCartItem = structuredClone(item)
+    newCartItem.description = value
+
+    const newCartItems = [...cartItems]
+    newCartItems[index] = newCartItem
+
+    setDescription(value)
+    setCartItems(newCartItems)
+  }
+
   return (
     <div className='flex-col border border-black bg-white'>
       <div className='flex justify-between items-center border bg-white'>
@@ -19,9 +43,10 @@ function CartMenu({ item, index }: CartMenuProps) {
       <div>
         <Input
           type='text'
-          value={item.description}
+          value={description}
           placeholder='備考'
           className='border-none bg-white rounded-none transition-all focus:bg-blue-50 focus:outline-none'
+          onChange={e => handleChange(e.target.value)}
         />
       </div>
     </div>
