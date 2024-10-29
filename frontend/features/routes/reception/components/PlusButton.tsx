03b734772dc3ba0a25670257bf2619fba641745b
akito-0520 <n.akito.h17@icloud.com>
2024-10-29 10:49:00 +0900

    feat: "使用しないので一旦コメントアウト"

--- frontend/features/routes/reception/components/PlusButton.tsx
+++ frontend/features/routes/reception/components/PlusButton.tsx
@@ -1,25 +1,25 @@
-import SkeletonButton from '@/components/skeletonButton'
-import type { Item } from '@/types/type'
-import { cartItemsAtom } from '@/utils/globalState'
-import { useSetAtom } from 'jotai'
-import { Plus } from 'lucide-react'
+// import SkeletonButton from '@/components/skeletonButton'
+// import type { Item } from '@/types/type'
+// import { cartItemsAtom } from '@/utils/globalState'
+// import { useSetAtom } from 'jotai'
+// import { Plus } from 'lucide-react'
 
-type plusButtonProp = {
-  item: Item
-}
+// type plusButtonProp = {
+//   item: Item
+// }
 
-export default function PlusButton({ item }: plusButtonProp) {
-  const setCartItems = useSetAtom(cartItemsAtom)
+// export default function PlusButton({ item }: plusButtonProp) {
+//   const setCartItems = useSetAtom(cartItemsAtom)
 
-  // Todo
-  // handlePlusItemをhooksにまとめる
-  const handleAddItem = () => {
-    setCartItems((prevItems: Item[]) => [...prevItems, item])
-  }
+//   // Todo
+//   // handlePlusItemをhooksにまとめる
+//   const handleAddItem = () => {
+//     setCartItems((prevItems: Item[]) => [...prevItems, item])
+//   }
 
-  return (
-    <SkeletonButton handleFunction={handleAddItem}>
-      <Plus className='h-6 w-6' />
-    </SkeletonButton>
-  )
-}
+//   return (
+//     <SkeletonButton handleFunction={handleAddItem}>
+//       <Plus className='h-6 w-6' />
+//     </SkeletonButton>
+//   )
+// }
