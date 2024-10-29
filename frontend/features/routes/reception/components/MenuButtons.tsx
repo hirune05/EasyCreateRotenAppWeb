f8bf26ab91fa24383e1189d1ee3f0e90de30c10d
f-yusei <fkys2932@gmail.com>
2024-10-30 02:50:14 +0900

    chore: format

--- frontend/features/routes/reception/components/MenuButtons.tsx
+++ frontend/features/routes/reception/components/MenuButtons.tsx
@@ -10,18 +10,18 @@ const MenuButtons = () => {
   }
   return (
     <div className='grid grid-cols-2 gap-3 h-full mx-auto '>
-      {storeItems.map((storeItem, index) => (
-        storeItem.status == 1 ? ( 
-        <>
-          <MenuButton storeItem={storeItem} key={index} isTicket={false} />
-          <MenuButton storeItem={storeItem} key={index} isTicket={true} />
-        </>
+      {storeItems.map((storeItem, index) =>
+        storeItem.status == 1 ? (
+          <>
+            <MenuButton storeItem={storeItem} key={index} isTicket={false} />
+            <MenuButton storeItem={storeItem} key={index} isTicket={true} />
+          </>
         ) : storeItem.status == 2 ? (
           <MenuButton storeItem={storeItem} key={index} isTicket={true} />
         ) : (
           <MenuButton storeItem={storeItem} key={index} isTicket={false} />
-        )
-      ))}
+        ),
+      )}
     </div>
   )
 }
