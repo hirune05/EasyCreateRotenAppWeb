e307bf6711ddf39aee5e1a1708fb647cc75c4a12
akito-0520 <n.akito.h17@icloud.com>
2024-10-29 15:43:51 +0900

    feat: "お試しでおいた"

--- frontend/features/routes/reception/components/ReceptionHome.tsx
+++ frontend/features/routes/reception/components/ReceptionHome.tsx
@@ -4,6 +4,7 @@ import ReceptionFooter from '@/features/routes/reception/components/ReceptionFoo
 import OrderDetailModal from '@/features/routes/reception/components/orderDetailmodal'
 import { Suspense } from 'react'
 import { useSetItemsByStoreId } from '../hooks'
+import ReportModal from '@/features/common/report/reportModal'
 export default function ReceptionHome() {
   const { isLoading, error } = useSetItemsByStoreId()
 
@@ -23,6 +24,7 @@ export default function ReceptionHome() {
         <div className='h-[80px]'>
           <MenuButtons />
         </div>
+        <ReportModal />
         <ReceptionFooter />
       </div>
     </div>
