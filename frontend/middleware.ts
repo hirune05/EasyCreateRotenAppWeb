172f2a081e9dcb0b7223d003d7789ff7605e56da
f-yusei <fkys2932@gmail.com>
2024-10-30 18:15:39 +0900

    fix: ログイン成功時に正常にミドルウェアが動作するように

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -33,7 +33,7 @@ export async function middleware(request: NextRequest) {
 
     const data = await response.json()
 
-    if (!data.valid) {
+    if (data.error) {
       throw new Error(data.error || 'Invalid token')
     }
 
